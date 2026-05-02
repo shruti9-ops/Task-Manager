b6138c15e753620fb3f4fcdf45d12de973282eb7
Shruti Khera <shrutikhera1432@gmail.com>
2026-05-02 17:02:34 +0530

    Fix signup link case sensitivity

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -73,7 +73,7 @@ function Login() {
 
         <p className="auth-footer">
           Don't have an account?{" "}
-          <a href="/signup">Create one</a>
+          <a href="/Signup">Create one</a>
         </p>
       </div>
     </div>

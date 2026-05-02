2aaaf9e219b7437504e959c87de89d2bba41745c
Shruti Khera <shrutikhera1432@gmail.com>
2026-05-02 17:06:44 +0530

    Update Login.jsx

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -73,7 +73,7 @@ function Login() {
 
         <p className="auth-footer">
           Don't have an account?{" "}
-          <a href="/signup">Create one</a>
+          <Link to="/signup">Create one</Link>
         </p>
       </div>
     </div>
